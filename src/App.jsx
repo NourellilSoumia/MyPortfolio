3ffafd4f178181e7cfd90c0ca2634730150ec0eb
NOUR-ELLIL <nourellilsoumia@gmail.com>
2025-02-18 00:33:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+function App() {
+
+    return (
+      <div className="m-40">
+       <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, World!
+      </h1>
+    </div>
+      </div>
+    )
+    }
+  export default App
