017b3c7e145fb1ebb383171ca0bbe5a64c1da554
NOUR-ELLIL <nourellilsoumia@gmail.com>
2025-02-18 01:17:51 +0100

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import React from 'react' ;
+import Header from './components/header';
+import Article from './components/article';
+import Footer from './components/footer';
+
 
 function App() {
-
-    return (
-      <div className="m-40">
-       <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Hello, World!
-      </h1>
-    </div>
-      </div>
-    )
-    }
-  export default App
+  return(
+    <>
+  
+  <Header/>
+  <Article/>
+  <Footer/>
+  
+    </>
+  )
+}
+export default App ;
