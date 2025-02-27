f9afbe8b089b0f988366d503ef42805f73deac41
NourellilSoumia <nourellilsoumia@gmail.com>
2025-02-27 01:59:12 +0000

    .32

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/Portfolio/', // Remplacez 'Portfolio' par le nom de votre dépôt GitHub
+  base: '/MyPortfolio/', // Remplacez 'Portfolio' par le nom de votre dépôt GitHub
   plugins: [react()],
 });
